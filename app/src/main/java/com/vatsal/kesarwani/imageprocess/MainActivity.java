20efe5e709d11720ade9d83341059193b10eaf4c
plazzy99 <vatsalkesarwani12@gmail.com>
2020-09-01 02:25:02 +0530

    ml integrated

--- app/src/main/java/com/vatsal/kesarwani/imageprocess/MainActivity.java
+++ app/src/main/java/com/vatsal/kesarwani/imageprocess/MainActivity.java
@@ -1,14 +1,127 @@
 package com.vatsal.kesarwani.imageprocess;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.label.ImageLabel;
+import com.google.mlkit.vision.label.ImageLabeler;
+import com.google.mlkit.vision.label.ImageLabeling;
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button image;
+    private TextView textt;
+    private ImageView pic;
+    private InputImage image1;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image = findViewById(R.id.image);
+        textt = findViewById(R.id.text);
+        pic = findViewById(R.id.pic);
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImagePicker.Companion.with(MainActivity.this)
+                        .crop()                    //Crop image(Optional), Check Customization for more option
+                        .compress(1024)            //Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)    //Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            //Image Uri will not be null for RESULT_OK
+            assert data != null;
+            Uri fileUri = data.getData();
+            pic.setImageURI(fileUri);
+
+            try {
+                image1 = InputImage.fromFilePath(getApplicationContext(), fileUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //You can get File object from intent
+            File file = ImagePicker.Companion.getFile(data);
+
+            //You can also get File Path from intent
+            String filePath = ImagePicker.Companion.getFilePath(data);
+
+            labeler();
+
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(this, ImagePicker.Companion.getError(data), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void labeler() {
+        // To use default options:
+        ImageLabeler labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
+
+        // Or, to set the minimum confidence required:
+        // ImageLabelerOptions options =
+        //     new ImageLabelerOptions.Builder()
+        //         .setConfidenceThreshold(0.7f)
+        //         .build();
+        // ImageLabeler labeler = ImageLabeling.getClient(options);
+
+        labeler.process(image1)
+                .addOnSuccessListener(new OnSuccessListener<List<ImageLabel>>() {
+                    @Override
+                    public void onSuccess(List<ImageLabel> labels) {
+                        // Task completed successfully
+                        // ...
+                        String s="";
+                        for (ImageLabel label : labels) {
+                            String text = label.getText();
+                            float confidence = label.getConfidence();
+                            int index = label.getIndex();
+
+                            Log.d(TAG,text+" "+confidence+" "+index);
+                            s+=text+" "+confidence+" "+index+"\n";
+                        }
+                        textt.setText(s);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Task failed with an exception
+                        // ...
+                    }
+                });
     }
 }
